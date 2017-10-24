e059781a8e9a1c12aecdd352652e98d0b7c0b05b
stab <sebastien@capsule5.com>
2017-10-24 15:58:36 +0200

    conf

---
+++ src/components/Examples.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div />
+    )
+  }
+}
+
+export default MyComponent

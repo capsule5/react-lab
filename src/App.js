e3d63946b192ea0ae55bb94bbbaaadcd727977b3
stab <sebastien@capsule5.com>
2017-10-24 16:02:30 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Animation with React</h1>
-        </header>
-        <div className="App-examples">
-          
-        </div>
-      </div>
-    );
-  }
-}
 
-export default App;
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={ logo } className="App-logo" alt="logo" />
+      <h1 className="App-title">Animation with React</h1>
+    </header>
+    <div className="App-examples" />
+  </div>
+)
+
+export default App
+

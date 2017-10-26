43b2bedfb9ea62048ba7059fdc3f70ca13a10b08
stab <sebastien@capsule5.com>
2017-10-26 16:04:15 +0200

    strcuture

---
+++ src/components/examples/ReactMotion.js
@@ -0,0 +1,35 @@
+import React, { PureComponent } from "react"
+import { Motion, spring } from "react-motion"
+import Example from "./Example"
+import { TAGS } from "../../utils/tags"
+
+const DATA = {
+  title: "React Motion",
+  tags: [
+    TAGS.animation,
+  ],
+  sources: [
+    "https://github.com/chenglou/react-motion",
+    "https://react.rocks/tag/react-motion",
+  ],
+}
+
+class ReactMotion extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <Example data={ DATA }>
+        <h3>Simple value animation</h3>
+        <Motion defaultStyle={ { x: 0 } } style={ { x: spring(10) } }>
+          {value => <div>{value.x}</div>}
+        </Motion>
+      </Example>
+    )
+  }
+}
+
+export default ReactMotion

c5b07260b5f99ce96d22f23eceb22f5dcf8b6896
stab <sebastien@capsule5.com>
2017-11-06 11:38:05 +0100

    feat(core): makes links list expendable

--- src/components/core/examples/example/Links.js
+++ src/components/core/examples/example/Links.js
@@ -6,9 +6,14 @@ const Wrapper = styled.div`
   padding:0;
   margin:0;
   li{
-
     list-style:none;
     margin-bottom:2px;
+    &.isExpendable{
+      display:none;
+      &.isOpen{
+        display:block;
+      }
+    }
   }
   a{
     text-decoration:none;
@@ -18,19 +23,56 @@ const Wrapper = styled.div`
       text-decoration:underline;
     }
   }
-`
-
-const Links = ({ links }) => (
-  <Wrapper className="data">
-    {
-      links.map(link => (
-        <li key={ link.id }>
-          <a href={ link.value } target="_blank" rel="noopener noreferrer" >{link.value}</a>
-        </li>
-      ))
+  .expendBtn{
+    cursor:pointer;
+    background:#AAA;
+    border:0;
+    color:#FFF;
+    padding:0px 4px 0px 4px;
+    font-size:11px;
+    display:inline-block;
+    margin-left:5px;
+    border-radius:3px;
+    &:hover{
+      background:#999;
     }
-  </Wrapper>
-)
+  }
+`
+class Links extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.expendList = this.expendList.bind(this)
+
+    this.state = {
+      isExpendable: this.props.links.length > 1,
+      isOpen: false,
+    }
+  }
+
+  expendList() {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+  
+  render() {
+    const { isExpendable, isOpen } = this.state
+
+    return (
+      <Wrapper className="data">
+        {
+          this.props.links.map((link, index) => (
+            <li key={ link.id } className={ `${isExpendable && index > 0 && "isExpendable"} ${isOpen && "isOpen"} ` }>
+              <a href={ link.value } target="_blank" rel="noopener noreferrer" >{link.value}</a>
+              {
+                isExpendable && index === 0 &&
+                <div onClick={ this.expendList } className="expendBtn">...</div>
+              }
+            </li>
+          ))
+        }
+      </Wrapper>
+    )
+  }
+}
 
 
 Links.defaultProps = {}

bddca3c87930bb8322eab192c88fda08fad87271
stab <sebastien@capsule5.com>
2017-11-13 14:31:16 +0100

    refactor(core): App in core

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
-import "./index.css"
-import App from "./App"
+import App from "./components/core/App"
 import registerServiceWorker from "./registerServiceWorker"
 import configStore from "./redux/configStore"
+import "./index.css"
 
 
 const store = configStore()
